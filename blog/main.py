fc9f98f7487dea26dc25b6b18057dd019b564db9
muhammedshabeen <muhammedshabeen982@gmail.com>
2024-07-02 21:49:18 +0530

    user model added

--- blog/main.py
+++ blog/main.py
@@ -3,10 +3,12 @@ from . import schemas
 from . import models
 from sqlalchemy.orm import Session
 from .database import engine, SessionLocal
+from typing import List
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -16,7 +18,7 @@ def get_db():
         db.close()
 
 
-@app.post('/create-blog',status_code=status.HTTP_201_CREATED)
+@app.post('/create-blog', status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -25,17 +27,65 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-
-@app.get('/get-blog')
+@app.get('/get-blog',response_model=List[schemas.ShowBlog])
 def get_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog-detail/{id}',status_code=200)
-def detail_blog(id,response: Response,db: Session = Depends(get_db)):
+@app.get('/blog-detail/{id}', status_code=200)
+def detail_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Blog with id {id} not found')
     return blog
+
+
+@app.delete('/delete-blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog_to_delete = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog_to_delete.first():
+        return {
+            "status": 0,
+            "message": "Blog not found"
+        }
+
+    blog_to_delete.delete(synchronize_session=False)
+    db.commit()
+    return {
+        "status": 1,
+        "message": " Blog deleted"
+    }
+
+
+@app.put('/update-blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog_to_update = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog_to_update.first():
+        return {
+            "status": 0,
+            "message": "Blog not found"
+        }
+
+    blog_to_update.update(request.dict())
+    db.commit()
+    return {
+        "status": 1,
+        "message": "Updated"
+    }
+
+
+
+
+
+
+@app.post('/create-user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
